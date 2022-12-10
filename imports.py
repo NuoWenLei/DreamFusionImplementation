df6c105813298c35c9327d55541efa33d5f57172
NuoWenLei <nuowen0612@gmail.com>
2022-12-10 16:19:00 -0500

    Update imports.py

--- imports.py
+++ imports.py
@@ -4,5 +4,5 @@ from PIL import Image
 from tqdm import tqdm
 from stable_diffusion_tf.constants import _UNCONDITIONAL_TOKENS
 from typing import Callable, Any
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
